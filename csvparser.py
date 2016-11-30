0a5de4ee51b4013172f7001bf2085ee5e7789978
kormin <abaotom14@gmail.com>
2016-11-30 14:24:06 +0800

    got subj lines

--- csvparser.py
+++ csvparser.py
@@ -6,9 +6,29 @@ class CsvParser():
 		# file = open(self.filename, 'r')
 		# print(file.read())
 		# file.close()
-		with open(self.filename) as f:
-			content = f.readlines()
-			print(content)
+
+		# with open(self.filename) as f:
+		# 	self.lines = f.readlines()
+		
+		self.lines = [line.rstrip('\n') for line in open(self.filename)]
+		self.length = len(self.lines)
+		# print(self.lines)
+		self.getSubject()
+
+	def getSubject(self):
+		if 'GROUP' in self.lines[3]:
+			del self.lines[self.length-2:self.length]
+			del self.lines[0:4]
+			self.printList()
+		else:
+			print('Error')
+
+	def printList(self):
+		print('\n'.join('{}: {}'.format(*k) for k in enumerate(self.lines)))
+
+	# usage: findWholeWord('seek')('those who seek shall find')
+	# def findWholeWord(self, word):
+	# 	return re.compile(r'\b({0})\b'.format(word), flags=re.IGNORECASE).search
 
 parse = CsvParser("StudentStudyLoad.csv")
 parse.readCsv()
