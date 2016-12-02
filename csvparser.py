039450ef511d36ed58237a62a74ff09517feb98a
kormin <abaotom14@gmail.com>
2016-12-02 12:14:20 +0800

    used csv parser for clean list

--- csvparser.py
+++ csvparser.py
@@ -1,3 +1,5 @@
+import csv
+
 class CsvParser():
 	def __init__(self, filename):
 		self.filename = filename
@@ -10,21 +12,34 @@ class CsvParser():
 		# with open(self.filename) as f:
 		# 	self.lines = f.readlines()
 		
-		self.lines = [line.rstrip('\n') for line in open(self.filename)]
-		self.length = len(self.lines)
-		# print(self.lines)
-		self.getSubject()
+		# self.lines = [line.rstrip('\n') for line in open(self.filename)]
 
-	def getSubject(self):
-		if 'GROUP' in self.lines[3]:
-			del self.lines[self.length-2:self.length]
-			del self.lines[0:4]
-			self.printList()
-		else:
-			print('Error')
+		with open(self.filename, 'r') as f:
+			reader = csv.reader(f)
+			self.lines = list(reader)
+		self.length = len(self.lines)
+		self.getSubjects()
+
+	def getSubjects(self):
+		# if 'GROUP' in self.lines[3]:
+		del self.lines[self.length-2:self.length]
+		del self.lines[0:4]
+		# str_list = filter(None, self.lines)
+		y = 0
+		for line in self.lines:
+			line = [x for x in self.lines[y] if x is not '']
+			line = [x for x in line if x is not ' ']
+			line = list(map(str.strip, line))
+			self.lines[y] = line
+			y+=1
+		self.length = len(self.lines)
+		self.printList()
+		# else:
+		# 	print('Error')
 
 	def printList(self):
 		print('\n'.join('{}: {}'.format(*k) for k in enumerate(self.lines)))
+		print("Length: ",(self.length))
 
 	# usage: findWholeWord('seek')('those who seek shall find')
 	# def findWholeWord(self, word):
